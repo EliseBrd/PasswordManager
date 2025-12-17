648088b1acdc75ec141db40a1b8a98872eca0725
Elise-44 <bouardelise.perso@gmail.com>
2025-12-17 09:32:04 +0100

    com fichiers create vault entry

--- PasswordManager.Web/Components/VaultEntries/CreateVaultEntryModal.razor.cs
+++ PasswordManager.Web/Components/VaultEntries/CreateVaultEntryModal.razor.cs
@@ -1,45 +1,45 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿//using Microsoft.AspNetCore.Components;
 
-namespace PasswordManager.Web.Components.VaultEntries
-{
-    public class CreateVaultEntryModal
-    {
-        [Parameter] public bool IsOpen { get; set; }
-        [Parameter] public EventCallback<bool> IsOpenChanged { get; set; }
+//namespace PasswordManager.Web.Components.VaultEntries
+//{
+//    public class CreateVaultEntryModal
+//    {
+//        [Parameter] public bool IsOpen { get; set; }
+//        [Parameter] public EventCallback<bool> IsOpenChanged { get; set; }
 
-        [Parameter] public EventCallback<VaultEntryCreateDto> OnSave { get; set; }
+//        [Parameter] public EventCallback<VaultEntryCreateDto> OnSave { get; set; }
 
-        string EntryName = "";
-        string EntryUsername = "";
-        string EntryPassword = "";
-        string EntryNotes = "";
+//        string EntryName = "";
+//        string EntryUsername = "";
+//        string EntryPassword = "";
+//        string EntryNotes = "";
 
-        async Task Close()
-        {
-            await IsOpenChanged.InvokeAsync(false);
-            Reset();
-        }
+//        async Task Close()
+//        {
+//            await IsOpenChanged.InvokeAsync(false);
+//            Reset();
+//        }
 
-        async Task Save()
-        {
-            var dto = new VaultEntryCreateDto
-            {
-                Name = EntryName,
-                Username = EntryUsername,
-                Password = EntryPassword,
-                Notes = EntryNotes
-            };
+//        async Task Save()
+//        {
+//            var dto = new VaultEntryCreateDto
+//            {
+//                Name = EntryName,
+//                Username = EntryUsername,
+//                Password = EntryPassword,
+//                Notes = EntryNotes
+//            };
 
-            await OnSave.InvokeAsync(dto);
-            await Close();
-        }
+//            await OnSave.InvokeAsync(dto);
+//            await Close();
+//        }
 
-        void Reset()
-        {
-            EntryName = "";
-            EntryUsername = "";
-            EntryPassword = "";
-            EntryNotes = "";
-        }
-    }
-}
+//        void Reset()
+//        {
+//            EntryName = "";
+//            EntryUsername = "";
+//            EntryPassword = "";
+//            EntryNotes = "";
+//        }
+//    }
+//}
