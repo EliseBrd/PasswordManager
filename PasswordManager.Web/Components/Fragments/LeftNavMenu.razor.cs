fbf5081e07deb48109ea74207967b9f164f0f689
Guillaume DUDOUET <guillaume.dudouet@iia-formation.fr>
2026-01-04 16:01:22 +0100

    add filter to vaults list

--- PasswordManager.Web/Components/Fragments/LeftNavMenu.razor.cs
+++ PasswordManager.Web/Components/Fragments/LeftNavMenu.razor.cs
@@ -1,14 +1,64 @@
 ﻿using Microsoft.AspNetCore.Components;
 using System.Security.Claims;
+using PasswordManager.Web.Services;
+using Microsoft.Identity.Web;
 
 namespace PasswordManager.Web.Components.Fragments;
 
 public partial class LeftNavMenu : ComponentBase
 {
     [Inject] private NavigationManager NavManager { get; set; } = default!;
+    [Inject] private VaultStateService VaultState { get; set; } = default!;
+    [Inject] private VaultService VaultService { get; set; } = default!;
+    [Inject] protected MicrosoftIdentityConsentAndConditionalAccessHandler ConsentHandler { get; set; } = default!;
+    
+    private int TotalCount { get; set; }
+    private int PersonalCount { get; set; }
+    private int SharedCount { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        await LoadCountsAsync();
+    }
+
+    private async Task LoadCountsAsync()
+    {
+        try
+        {
+            // On récupère tous les vaults pour calculer les compteurs
+            var allVaults = await VaultService.GetAccessibleVaultsAsync(null);
+            if (allVaults != null)
+            {
+                var vaults = allVaults.ToList();
+                TotalCount = vaults.Count;
+                PersonalCount = vaults.Count(v => !v.IsShared);
+                SharedCount = vaults.Count(v => v.IsShared);
+            }
+        }
+        catch (MicrosoftIdentityWebChallengeUserException ex)
+        {
+            ConsentHandler.HandleException(ex);
+        }
+        catch (Exception)
+        {
+            // Ignorer les erreurs silencieusement pour le menu
+        }
+    }
+
     void GoToCreateVault()
     {
         NavManager.NavigateTo("/createVault");
+    }
+    
+    private void FilterVaults(bool? isShared)
+    {
+        VaultState.SetFilter(isShared);
+        
+        // Si on n'est pas sur la page d'accueil, on redirige
+        if (NavManager.Uri != NavManager.BaseUri)
+        {
+            NavManager.NavigateTo("/");
+        }
     }
     
     string GetDisplayName(ClaimsPrincipal user)
